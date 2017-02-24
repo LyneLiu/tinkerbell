d0dfe6b8a495968f7799f7a7d905320fb80fc80c
LyneLiu <595596226@qq.com>
2017-02-24 18:53:41 +0800

    选择thymeleaf作为模板引擎，后期可以使用spring boot + thymeleaf + angularjs/bootstrapjs实现web开发。

--- src/main/java/org/mauritius/controller/BaseController.java
+++ src/main/java/org/mauritius/controller/BaseController.java
@@ -1,5 +1,6 @@
 package org.mauritius.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by nn_liu on 2017/2/24.
  */
 
-@RestController
+/**
+ * @RestController 和 @Controller的区别：
+ * 1、@RestController = @ResponseBody + @Controller；
+ * 2、只有使用@Controller和视图解析器InternalResourceViewResolver，才可以实现模板渲染；
+ * 3、@ResponseBody会返回JSON，XML或自定义mediaType内容到页面。
+ */
+@Controller
 public class BaseController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/home")
     public String index(){
-        return "Index Page!";
+        return "index";
     }
 
 }
