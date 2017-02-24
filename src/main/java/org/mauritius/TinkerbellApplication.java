7102e7b606e7285056cddedd5e057c135bbb9b54
LyneLiu <595596226@qq.com>
2017-02-24 17:59:07 +0800

    add .gitignore files

---
+++ src/main/java/org/mauritius/TinkerbellApplication.java
@@ -0,0 +1,12 @@
+package org.mauritius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkerbellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkerbellApplication.class, args);
+	}
+}
