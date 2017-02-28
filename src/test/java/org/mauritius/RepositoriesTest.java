cd043386ca5ef83e56195adf628952a3d33137cb
LyneLiu <595596226@qq.com>
2017-02-28 10:50:02 +0800

    问题：问什么test操作会删除数据库中的表？？？

--- src/test/java/org/mauritius/RepositoriesTest.java
+++ src/test/java/org/mauritius/RepositoriesTest.java
@@ -12,12 +12,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
- * @Commit 注解提交测试；@Rollback 注解回滚测试。
+ * @Commit 注解提交测试; @Rollback 注解回滚测试。
  * http://stackoverflow.com/questions/29640989/spring4-junit4-test-transactionconfiguration-vs-transactional-use-only-one-o
  *
  * Created by nn_liu on 2017/2/27.
@@ -26,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Commit
 @EnableAutoConfiguration
 public class RepositoriesTest {
     @Autowired
@@ -36,13 +35,21 @@ public class RepositoriesTest {
 
     @Test
     public void testUserRepositories() {
+
         User user = userRepository.findByFirstName("spring");
-        Assert.assertEquals(2, userRepository.count());
+
+        User user1 = new User();
+        user1.setFirstName("lyne");
+        user1.setLastName("liu");
+        user1.setPassword("123456");
+
+        userRepository.save(user1);
+        Assert.assertEquals(3, userRepository.count());
     }
 
     @Test
-    @Commit
     public void testOwnerRepositories() {
+
         Owner owner = new Owner();
         owner.setOwnerName("lyne");
         owner.setOwnerAge("20");
