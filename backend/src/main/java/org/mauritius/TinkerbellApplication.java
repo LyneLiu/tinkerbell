8ebab1866d6ee66ece62d364cc8cb0788f60b644
LyneLiu <595596226@qq.com>
2017-03-13 11:22:45 +0800

    format the code

--- backend/src/main/java/org/mauritius/TinkerbellApplication.java
+++ backend/src/main/java/org/mauritius/TinkerbellApplication.java
@@ -6,7 +6,6 @@ import org.mauritius.tinkerbell_security.config.SecurityJPAManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
--- backend/src/main/java/org/mauritius/tinkerbell_portal/controller/BaseController.java
+++ backend/src/main/java/org/mauritius/tinkerbell_portal/controller/BaseController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by nn_liu on 2017/2/24.
